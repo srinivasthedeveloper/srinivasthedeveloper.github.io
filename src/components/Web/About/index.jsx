67c08be0d5daeebe6aebfd5e9c246030797e7990
srinivasthedeveloper <srinivasthedeveloper@gmail.com>
2023-01-06 00:07:53 +0530

    added about content without designs

--- src/components/Web/About/index.jsx
+++ src/components/Web/About/index.jsx
@@ -24,7 +24,16 @@ export default function About({
 
     return (
         <section className={`${styles['container']}`} ref={aboutRef}>
-            <span className={`${styles['header']}`}>About</span>
+            <header>About Me</header>
+            <span>I'm srinivasTheDeveloper, friendly and socializing person also enjoys being alone with a computer.(used to create chat-bots and voice-assistants in boredom)</span>
+            <span>A Computerphile who likes to code even 12+ hrs straight when things got interesting.</span>
+            <span>Friendly Languages Js(React, Next, React-Native, Node), Python(Django)</span>
+            <span>Languages worked on Java,C++,C</span>
+            <span>Simple, Sportive and creative mindset regardless of situation is my 2nd major strength</span>
+            <span>my weakness is that I easily dive deep into the current task i'm working on which is quite unhealthy when it involves sitting long time in a hard seat without a break.</span>
+            <span>(but don't worry i'm working on some pomodoro breaks and bought a fluffy cushion for starters)</span>
+            <span>My hobbies are watching(anime,series,tech talks) listening(songs,podcasts) singing(don't worry you won't have access to my bathroom) reading(comics,books,blogs) writing/typing(crazy code snips, typing games, storeis)</span>
+            <button>Wanna know more?</button>
         </section>
     )
 }
