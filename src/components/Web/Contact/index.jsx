d64427c7b393b6dd5639b69e696ae314624d199a
srinivasthedeveloper <srinivasthedeveloper@gmail.com>
2023-01-13 20:28:02 +0530

    fixed whatido flex wrap

--- src/components/Web/Contact/index.jsx
+++ src/components/Web/Contact/index.jsx
@@ -3,28 +3,33 @@ import useOnScreen from "Utils/useOnScreen";
 import styles from "./styles.module.scss";
 
 export default function Contact({
-    activeNav="",
-    setActiveNav=()=>{},
+    activeNav = "",
+    setActiveNav = () => { },
     ...props
 }) {
     const contactRef = useRef();
     const isVisible = useOnScreen(contactRef);
 
-    useEffect(()=>{
-        if(isVisible && activeNav!=='Contact'){
+    useEffect(() => {
+        if (isVisible && activeNav !== 'Contact') {
             setActiveNav("Contact");
         }
-    },[isVisible])
+    }, [isVisible])
 
-    useEffect(()=>{
-        if(activeNav==='Contact'){
+    useEffect(() => {
+        if (activeNav === 'Contact') {
             contactRef.current.scrollIntoView();
         }
-    },[activeNav])
+    }, [activeNav])
 
     return (
         <section className={`${styles['container']}`} ref={contactRef}>
             <span className={`${styles['header']}`}>Contact</span>
+            <div className={`${styles['form-container']}`}>
+                <input className={`${styles['subject']}`} />
+                <textarea className={`${styles['body']}`} />
+                <a href="#" className={`${styles['submit']}`}>Sent Message</a>
+            </div>
         </section>
     )
 }
