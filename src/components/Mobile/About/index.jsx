6af95828db64886a63f879e46a7456285e39ce8c
srinivasthedeveloper <srinivasthedeveloper@gmail.com>
2023-02-03 20:32:16 +0530

    added some content for about page

--- src/components/Mobile/About/index.jsx
+++ src/components/Mobile/About/index.jsx
@@ -17,6 +17,17 @@ export default function MAbout({ activeNav, setActiveNav = () => { } }) {
 
     return (
         <div className={styles['container']} ref={aboutRef} id={'About-mview'}>
+            <div>
+                <span>Hello Buddy...👋</span>
+            </div>
+            <div>
+                <span>I'm Srinivas.K (The Developer)</span>
+                <span>A Computerphile and also a people person</span>
+                <span>Front-end Developer (web,mobile)</span>
+                <span>ReactJs, ReactNative are my favorite frameworks</span>
+                <span>Python, Javascript are my favorite languages</span>
+                <span>I use django, Node js for backend development</span>
+            </div>
         </div>
     )
 }
