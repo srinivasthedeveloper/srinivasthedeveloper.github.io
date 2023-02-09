26d6141c7a3a631673efd83ed9515d73a042cdbf
srinivasthedeveloper <srinivasthedeveloper@gmail.com>
2023-02-09 21:52:42 +0530

    added resume design for mobile

---
+++ src/components/Mobile/Resume/index.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef } from "react";
+
+import useOnScreen from "Utils/useOnScreen";
+import styles from "./styles.module.scss";
+import resumePdf from "assets/pdf/resume.pdf";
+import page1 from "assets/image/resume/1.png";
+import page2 from "assets/image/resume/2.png";
+
+
+export default function MResume({ activeNav, setActiveNav = () => { } }) {
+
+    const ResumeRef = useRef();
+    const isVisible = useOnScreen(ResumeRef);
+
+    useEffect(() => {
+        if (isVisible && activeNav !== 'Resume') {
+            setActiveNav("Resume");
+        }
+    }, [isVisible])
+
+
+    return (
+        <div className={styles['container']} ref={ResumeRef} id={'Resume-mview'}>
+            <span className={`${styles['header']}`}>Resume</span>
+            <div className={`${styles['resume-container']}`}>
+                <img
+                    src={page1}
+                    loading="eager"
+                    alt={"Resume Page 1"}
+                    title={"Resume Page 1"}
+                />
+                <img
+                    src={page2}
+                    loading="eager"
+                    alt={"Resume Page 2"}
+                    title={"Resume Page 2"}
+                />
+            </div>
+
+            <div className={styles['button-container']}>
+                <a href={"resumePdf"}
+                    target="_blank" rel="noreferrer"
+                    title="download resume"
+                    download="Srinivas_K (The Developer) Resume.pdf"
+                    className={`${styles['download-btn']}`}
+                >Download Resume</a>
+            </div>
+        </div>
+    )
+}
